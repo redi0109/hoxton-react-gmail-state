fec3c87bc6d5762f46f15e6d843605abc4e105e5
Demir Boraj <demirboraj97@gmail.com>
2022-07-27 17:41:47 +0200

    123

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,46 @@ import initialEmails from "./data/emails";
 
 import "./App.css";
 
+import { useState } from "react";
+
+type Email = {
+  id: number;
+  sender: string;
+  title: string;
+  starred: boolean;
+  read: boolean;
+};
+
 function App() {
-  // Use initialEmails for state
-  
-  console.log(initialEmails);
+  const [emails, setEmails] = useState(initialEmails);
+
+  function toogleRead(email: Email) {
+    // 1- klonojme te dhenat
+    const emailsCopy = structuredClone(emails);
+
+    //2- perputhim te dhenat qe na duhen e "emails" me "emailsCopy"
+    // @ts-ignore
+    const match = emailsCopy.finde((target) => target.id === email.id);
+
+    match.read = !match.read;
+
+    // updetojme te dhenat
+    setEmails(emailsCopy);
+  }
+
+  function toogleStarred(email: Email) {
+    // 1- klonojme te dhenat
+    const emailsCopy = structuredClone(emails);
+
+    //2- perputhim te dhenat e "emails" me "emailsCopy"
+     // @ts-ignore
+    const match = emailsCopy.finde((target) => target.id === email.id);
+
+    match.starred = !match.starred;
+
+    // updetojme te dhenat
+    setEmails(emailsCopy);
+  }
 
   return (
     <div className="app">
@@ -40,16 +76,32 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{
-        initialEmails.map(email => (
-          <div className="email">
-            <input type="checkbox" />
-            <input className="star-checkbox" type="checkbox" />
+      <main className="emails">
+        {emails.map((email) => (
+          <div className={email.read ? 'email read' : 'email unread'}>
+            <input
+              className='read-checkbox'
+              type='checkbox'
+              checked={email.read}
+              // te bejme te mundur klikimin e checkboxit te pare
+              onClick={() => {
+                toogleRead(email);
+              }}
+            />
+            <input
+              className="star-checkbox"
+              type="checkbox"
+              checked={email.starred}
+              // te bejme te mundur klikimin e checkboxit te paare
+              onClick={() => {
+                toogleStarred(email);
+              }}
+            />
             <span>{email.sender}</span>
             <span className="title">{email.title}</span>
           </div>
-        ))
-      }</main>
+        ))}
+      </main>
     </div>
   );
 }
